0c267375235764cacb63960f3a65c6c530d51272
mguler <murat.guler@lycos.com>
2016-01-19 09:20:26 +0200

    sms console uygulaması güncellendi , sms gönderimi için gerekli olan bilgilerin console ekranından giriş yapılması sağlandı

--- SmsSender.ConsoleUI/Program.cs
+++ SmsSender.ConsoleUI/Program.cs
@@ -2,6 +2,7 @@
 using SmsSender.Common;
 using System.IO;
 using System.Diagnostics;
+using System.Linq;
 
 namespace SmsSender.ConsoleUI
 {
@@ -10,27 +11,51 @@ namespace SmsSender.ConsoleUI
         static void Main(string[] args)
         {
             var parameters = new string[] { "", "", "" };
-            var providers = SmsSenderProviderHelper.CreateInstance<BaseSmsProvider>("", "", "");
+            var providers = SmsSenderProviderHelper.CreateInstance<BaseSmsProvider>("", "", "").ToList();
+            Console.WriteLine("Provider Seçiniz");
+
             foreach (var provider in providers)
             {
-                Console.WriteLine(provider.ProviderName);
-                var results = provider.SendSms(new[] { new SmsRequest()
-                {
-                    Number = "905422476935",
-                    Content = "Merhaba"
-                },new SmsRequest()
-                {
-                    Number = "90542247693",
-                    Content = "Me"
-                } });
-                foreach (var result in results)
-                {
-                    //Console.WriteLine(result);
-                    File.AppendAllText("result.txt", "\n" + result.ToString());
-                }
+                Console.WriteLine("{0} - {1}", providers.IndexOf(provider) + 1, provider.ProviderName);
             }
-            Process.Start("result.txt");
-            Console.ReadLine();
+
+            string selection = Console.ReadLine();
+            int index = 0;
+
+            if (!int.TryParse(selection, out index))
+            {
+                Console.WriteLine("Geçersiz seçim");
+                return;
+            }
+
+            if (index <= 0 || index > providers.Count)
+            {
+                Console.WriteLine("Geçersiz seçim");
+                return;
+            }
+
+            Console.WriteLine("SMS provider kullanıcı adı giriniz");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("SMS provider şifre giriniz");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("Mesaj başlığını giriniz");
+            string header = Console.ReadLine();
+
+            Console.WriteLine("Telefon numarası giriniz");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine("Göndermek istediğiniz mesajı giriniz");
+            string messageContent = Console.ReadLine();
+
+            index--;
+            BaseSmsProvider selectedProvider = SmsSenderProviderHelper.CreateInstance(providers[index].ProviderName, username, password, header);
+            var smsResponses = selectedProvider.SendSms(new SmsRequest() { Content = messageContent, Number = phoneNumber });
+
+
+            foreach (var smsResponse in smsResponses) Console.WriteLine(smsResponse.ProviderMessage);
+
+            Console.ReadKey();
         }
 
     }
