7bfba747d065c06259316201b1891e76374c609b
ilkerhalil <ilkerhalil@gmail.com>
2016-01-19 10:16:35 +0200

    Bootstrap eklendi. Proje versiyonları .net 4.6'ya yükseltildi.

---
+++ SmsSender.IOC/BootStrap.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.Practices.Unity;
+using SmsSender.Avea;
+using SmsSender.Turkcell;
+using SmsSender.Vodafone;
+
+namespace SmsSender.IOC
+{
+    public static class BootStrap
+    {
+        public static IUnityContainer Init()
+        {
+            var unityContainer = new UnityContainer();
+            unityContainer.RegisterType<ISmsProvider, AveaSmsProvider>("aveaSmsProvider");
+            unityContainer.RegisterType<ISmsProvider, VodafoneSmsProvider>("vodafoneSmsProvider");
+            unityContainer.RegisterType<ISmsProvider, TurkcellSmsProvider>("turkcellSmsProvider");
+            return unityContainer;
+        }
+    }
+}
